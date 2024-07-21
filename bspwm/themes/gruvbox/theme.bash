ad590a4b08997718a5c10b195086ae740b0f0676
m-breno <breno_p@tuta.io>
2024-07-20 21:31:12 -0300

    Initial commit

---
+++ bspwm/themes/gruvbox/theme.bash
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Default Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#1D2021'
+foreground='#D4BE98'
+color0='#3C3836'
+color1='#EA6962'
+color2='#A9B665'
+color3='#D8A657'
+color4='#7DAEA3'
+color5='#D3869B'
+color6='#89B482'
+color7='#D4BE98'
+color8='#504945'
+color9='#EA6962'
+color10='#A9B665'
+color11='#D8A657'
+color12='#7DAEA3'
+color13='#D3869B'
+color14='#89B482'
+color15='#DDC7A1'
+
+accent='#D8A657'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrainsMono Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='JetBrains Mono 10'
+rofi_icon='Arc-Circle'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Arc-Dark'
+icon_theme='Papirus-Dark'
+cursor_theme='Qogirr'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x10'
+dunst_origin='top-right'
+dunst_font='JetBrainsMono Nerd Font 9'
+dunst_border='2'
+dunst_separator='1'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
